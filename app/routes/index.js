a486d99cc9b6ee4b23d9265753c54a8e8f41070c
nicolas <nickh93@outlook.com>
2016-11-01 10:22:37 -0400

    add API key and .env file

---
+++ app/routes/index.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+
+export default Ember.Route.extend({
+  actions: {
+    zipLookup(params) {
+      this.transitionTo('results', params.zip);
+    }
+  }
+});
---
+++ app/routes/results.js
@@ -0,0 +1,13 @@
+import config from '../config/environment';
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+ model: function(params) {
+   var key = config.myApiKey;
+   console.log(key);
+   var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=' +key+ '&zip=' +params.zip;
+   return Ember.$.getJSON(url).then(function(responseJSON) {
+     return responseJSON.results;
+   });
+  }
+});
